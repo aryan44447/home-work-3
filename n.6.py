f3483824a80702cb67bc539b730d3da2d61dc5de
aryan44447 <100072140+aryan44447@users.noreply.github.com>
2022-02-28 00:38:01 +0330

    Add files via upload

---
+++ n.6.py
@@ -0,0 +1,13 @@
+x = 2
+
+
+print("x")#it'll be printed.
+print('x')#it'll be printed
+#because [print] command  exactly shows  whatever is into the quotation mark
+print(x)#it'll be printed
+# this command prints directly x value
+print("x+1")#it'll be printed
+#because [print] command  exactly shows  whatever is into the quotation mark
+print('x value be summed with 1',' =' ,x+1)#it'll be printed
+
+'''print("x"+1)can only concatenate str not "int" to str| an error occured across running this program codage'''
