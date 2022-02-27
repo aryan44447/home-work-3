f226d2ea5be0becbfdf94a447c36016b5d1cb944
aryan44447 <100072140+aryan44447@users.noreply.github.com>
2022-02-28 00:42:56 +0330

    Update n.6.py

--- n.6.py
+++ n.6.py
@@ -10,4 +10,4 @@ print("x+1")#it'll be printed
 #because [print] command  exactly shows  whatever is into the quotation mark
 print('x value be summed with 1',' =' ,x+1)#it'll be printed
 
-'''print("x"+1)can only concatenate str not "int" to str| an error occured across running this program codage'''
+'''print("x"+1)can only concatenate str not "int" to str| an error occurred across running this program code'''
